f9d6e7ee806bfa5826095c1cb91692fd450b9af7
dreamtoapp <dreamtoapp@gmail.com>
2025-05-26 16:41:34 +0300

    tsc Clear no Error 1

--- app/dashboard/orders-mangment/component/action/msgCounter.ts
+++
@@ -1,7 +0,0 @@
-// Stub for notifcationMsg
-// TODO: Implement actual notification message logic
-
-export const notifcationMsg = () => {
-  // Placeholder implementation
-  return 'Notification message';
-};
